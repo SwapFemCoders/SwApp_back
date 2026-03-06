7d06df8d0445c8079b41ac7f48c949c5c10be583
Nuclea88 <ingrid.lopov@gmail.com>
2026-03-06 20:05:56 +0100

    feat: SWAP-27 Create method create and gets in service

--- src/main/java/com/swapp/swapp/service/UserService.java
+++ src/main/java/com/swapp/swapp/service/UserService.java
@@ -1,12 +1,25 @@
 package com.swapp.swapp.service;
 
+import java.util.List;
+
+import com.swapp.swapp.dto.request.UserRequestDTO;
+import com.swapp.swapp.dto.response.UserBasicResponseDTO;
+import com.swapp.swapp.dto.response.UserProfileResponseDTO;
 import com.swapp.swapp.entity.User;
 
 public interface UserService {
 
-    public User createUser (User user);
+    public UserProfileResponseDTO createUser (UserRequestDTO user);
 
     public User getUserById (int id);
+
+    public UserProfileResponseDTO getFullUserById (int id);
+
+    public UserBasicResponseDTO getBasicUserById (int id);
+
+    public List<String> getAllUserNames();
+
+    public UserBasicResponseDTO getUserByEmail(String email);
 
     public User updateUser (User user);
 
--- src/main/java/com/swapp/swapp/service/UserServiceImpl.java
+++ src/main/java/com/swapp/swapp/service/UserServiceImpl.java
@@ -1,20 +1,54 @@
 package com.swapp.swapp.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.swapp.swapp.dto.request.UserRequestDTO;
+import com.swapp.swapp.dto.response.UserBasicResponseDTO;
+import com.swapp.swapp.dto.response.UserProfileResponseDTO;
 import com.swapp.swapp.entity.User;
+import com.swapp.swapp.mapper.UserMapper;
+import com.swapp.swapp.repository.UserRepository;
 
 public class UserServiceImpl implements UserService{
 
-    @Override
-    public User createUser(User user) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
-    public User getUserById(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getUserById'");
+    public UserProfileResponseDTO createUser(UserRequestDTO userIn) {
+        User user = userMapper.toEntity(userIn);
+        user.setPoints(3);
+        userRepository.save(user);
+        return userMapper.toProfileResponse(user);
     }
+    @Override
+    public User getUserById (int id){
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty())
+            throw new RuntimeException("The user not exist");
+
+        return optionalUser.get();
+    }
+
+    @Override
+    public UserProfileResponseDTO getFullUserById(int id) {
+        return userMapper.toProfileResponse(getUserById(id));
+    }
+
+    @Override
+    public UserBasicResponseDTO getBasicUserById (int id){
+        return userMapper.toBasicResponse(getUserById(id));
+    }
+
+
+
+    //A PARTIR D AQUI HAY Q REVISAR
 
     @Override
     public User updateUser(User user) {
@@ -32,6 +66,18 @@ public class UserServiceImpl implements UserService{
     public void deleteUser(int id) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
+    }
+
+    @Override
+    public List<String> getAllUserNames() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllUserNames'");
+    }
+
+    @Override
+    public UserBasicResponseDTO getUserByEmail(String email) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUserByEmail'");
     }
 
 }
